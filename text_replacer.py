b35228a33061986eede16ffe4159a2db1e3cf86f
pacoisenberg <francisco.isenberg@gmail.com>
2022-05-01 13:00:33 -0700

    finished script

--- text_replacer.py
+++ text_replacer.py
@@ -1,12 +1,13 @@
 import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-base_go_link = "https://go/"
+base_go_link = "http://go/edit/"
 part450_sections = []
 part450_url = []
 
-with open('short_part450list.csv','r') as f:
+with open('part450list.csv','r') as f:
     csv_data = csv.reader(f)
 
     for row in csv_data:
@@ -14,18 +15,28 @@ with open('short_part450list.csv','r') as f:
         part450_url.append(row[3])
 
 section_count = len(part450_url)
-driver = webdriver.Chrome()
 
-for num in range(section_count):
-    go_450_url = base_go_link+part450_sections[num]
-    print(go_450_url)
+try:
+    driver = webdriver.Chrome()
+    print("Chrome is open")
 
-    driver.get(go_450_url)
-    driver.implicitly_wait(0.5)
-    text_box = driver.find_element(By.NAME, "?????")
-    button = driver.find_element(By.NAME, "????")
+    for num in range(section_count):
+        go_450_url = base_go_link+part450_sections[num]
 
-    text_box.send_keys(part450_url[num])
-    button.click()
+        driver.get(go_450_url)
+        driver.implicitly_wait(0.5)
+        text_box = driver.find_element(By.ID,"url")
+
+        print(go_450_url)
+
+        text_box.clear()
+        text_box.send_keys(part450_url[num])
+        text_box.send_keys(Keys.ENTER)
+
+except Exception as err:
+    print(err)
+    driver.quit()
 
 driver.quit()
+
+print("And it's done.")
