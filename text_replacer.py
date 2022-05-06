4a404dac29a72016f8c3fe35d2c7adb2b2c588f4
pacoisenberg <francisco.isenberg@gmail.com>
2022-05-06 14:14:39 -0700

    updated script to use 420 csv and generalize variable names. note 450 csv has an extra column and no longer works

--- text_replacer.py
+++ text_replacer.py
@@ -4,33 +4,33 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 base_go_link = "http://go/edit/"
-part450_sections = []
-part450_url = []
+cfr_part_sections = []
+cfr_part_url = []
 
-with open('part450list.csv','r') as f:
+with open('part420list.csv','r') as f:
     csv_data = csv.reader(f)
 
     for row in csv_data:
-        part450_sections.append(row[0])
-        part450_url.append(row[3])
+        cfr_part_sections.append(row[0])
+        cfr_part_url.append(row[2])
 
-section_count = len(part450_url)
+section_count = len(cfr_part_url)
 
 try:
     driver = webdriver.Chrome()
     print("Chrome is open")
 
     for num in range(section_count):
-        go_450_url = base_go_link+part450_sections[num]
+        go_cfr_part_url = base_go_link+cfr_part_sections[num]
 
-        driver.get(go_450_url)
+        driver.get(go_cfr_part_url)
         driver.implicitly_wait(0.5)
         text_box = driver.find_element(By.ID,"url")
 
-        print(go_450_url)
+        print(cfr_part_url[num])
 
         text_box.clear()
-        text_box.send_keys(part450_url[num])
+        text_box.send_keys(cfr_part_url[num])
         text_box.send_keys(Keys.ENTER)
 
 except Exception as err:
